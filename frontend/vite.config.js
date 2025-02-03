db710f36f7d57c820b4f221987ad0b54925dbe15
nasim0994 <nasimuddin0994@gmail.com>
2025-02-03 09:56:20 +0600

    update again

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 3500,
+    chunkSizeWarningLimit: 2000,
   },
   server: {
     port: 3800,
